5c4c4910b33269e65d3e5ebc547188ee3dae9bc9
IvEp <15ivini15@gmail.com>
2025-10-16 18:09:12 +0200

    Add files via upload

---
+++ webtech/src/test/java/webtech_semester_4/webtech/WebtechApplicationTests.java
@@ -0,0 +1,13 @@
+package webtech_semester_4.webtech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebtechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
